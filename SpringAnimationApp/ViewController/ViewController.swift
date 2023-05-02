0c39491c884e03c1740d7c6078fcc3b19ebb0590
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-02 21:09:24 +0500

    Work with StoryBoard

--- SpringAnimationApp/ViewController/ViewController.swift
+++ SpringAnimationApp/ViewController/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var springView: SpringView!
+    
+    @IBOutlet var presetLabal: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var deleyLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func runButoonTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
