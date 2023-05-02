3f270b9fcce9024211e61e4c23cca42d526d98e7
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-02 20:38:05 +0500

    Add model and data store

--- SpringAnimationApp/Models/Animation.swift
+++ SpringAnimationApp/Models/Animation.swift
@@ -5,4 +5,18 @@
 //  Created by Sergei Bakhmatov on 02.05.2023.
 //
 
-import Foundation
+struct Animation {
+    
+    let preset: [String]
+    let curve: [String]
+    
+    static func getPresetList() -> Animation {
+        
+        let animations = Animation(
+            preset: DataStore.shared.presets,
+            curve: DataStore.shared.curves
+        )
+        
+        return animations
+    }
+}
