292d0e7499dbfde0676fb8b9602504265c79f9e9
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-05-03 19:21:31 +0500

    Work with View Controller

--- SpringAnimationApp/ViewController/ViewController.swift
+++ SpringAnimationApp/ViewController/ViewController.swift
@@ -8,26 +8,62 @@
 import UIKit
 import SpringAnimation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     @IBOutlet var springView: SpringView!
     
     @IBOutlet var presetLabal: UILabel!
     @IBOutlet var curveLabel: UILabel!
-    @IBOutlet var forceLabel: UILabel!
-    @IBOutlet var durationLabel: UILabel!
-    @IBOutlet var deleyLabel: UILabel!
     
+    private let animations = Animation.getPresetList()
+    
+    private var animationStarted = false
+    private var currentPreset = ""
+    private var currentCurve = ""
+    private var nextPreset = ""
+    private var nextCurve = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentPreset = animations.preset.randomElement() ?? ""
+        currentCurve = animations.curve.randomElement() ?? ""
+        
+        presetLabal.text = "Preset: \(currentPreset)"
+        curveLabel.text = "Curve: \(currentCurve)"
+
     }
     
     
     @IBAction func runButoonTapped(_ sender: UIButton) {
-    }
-    
+        
+        if !animationStarted {
+            springView.animation = currentPreset
+            springView.curve = currentCurve
+            springView.animate()
+            animationStarted.toggle()
+            
+            nextPreset = animations.preset.randomElement() ?? ""
+            nextCurve = animations.curve.randomElement() ?? ""
+            
+        } else {
+            currentPreset = nextPreset
+            currentCurve = nextCurve
+            
+            presetLabal.text = "Preset: \(currentPreset)"
+            curveLabel.text = "Curve: \(currentCurve)"
+            
+            springView.animation = currentPreset
+            springView.curve = currentCurve
+            springView.animate()
+            
+            nextPreset = animations.preset.randomElement() ?? ""
+            nextCurve = animations.curve.randomElement() ?? ""
+        }
+        
+        sender.setTitle("Run \(nextPreset)", for: .normal)
 
+    }
 }
+
 
